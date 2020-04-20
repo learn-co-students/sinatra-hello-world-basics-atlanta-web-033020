31b36fd906eac5cbb3f2fd0a2993fe89735cbf20
rpdecks <rpdecks@gmail.com>
2020-04-19 23:11:02 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
 
   get '/' do
     "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Robert"
+  end 
+
+  get '/hometown' do
+    "My hometown is Decatur, GA."
+  end 
+
+  get '/favorite-song' do
+    "My favorite song is You Are My Sunshine."
   end 
 
 end
